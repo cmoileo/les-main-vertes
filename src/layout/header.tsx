260b00fb886e8e2fd3b0f23b7a9591cd283ea102
cmoileo <leo.fezard33@gmail.com>
2024-06-17 15:13:55 +0200

    add header+footer

---
+++ src/layout/header.tsx
@@ -0,0 +1,33 @@
+import getMenu from "../hook/menus";
+import { MenuItem } from "../types/types";
+
+export default async function MenuComponent() {
+    const menu = await getMenu("top-left-1")
+
+    return (
+        <header>
+            <nav className="menu">
+                {menu.length > 0 && menu.map((item: MenuItem, i: number) => (
+                    <ul key={i}>
+                        <a href={item.url}>
+                            <li>{item.title}</li>
+                        </a>
+                        {
+                            item.children.length > 0 && (
+                                <div className="sub-menu">
+                                    {
+                                        item.children.map((child: MenuItem, i:number) => (
+                                            <a key={i} href={child.url}>
+                                                <li>{child.title}</li>
+                                            </a>
+                                        ))
+                                    }
+                                </div>
+                            )
+                        }
+                    </ul>
+                ))}
+            </nav>
+        </header>
+    );
+}

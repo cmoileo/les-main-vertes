f6d50cc48cd18784ff91d701ed4ee589142b158a
cmoileo <leo.fezard33@gmail.com>
2024-06-17 15:12:25 +0200

    add hooks

---
+++ src/hook/getPost.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { Post } from "../types/types";
+
+export default async function getPostData(prop: string) {
+    const res = await axios.get(`${process.env.WP_URL}/wp-json/wp/v2/posts?slug=${prop}`);
+    const postData = res.data[0];
+    console.log(postData)
+    const title = postData.title.rendered;
+    const content = postData.content.rendered;
+    const date = postData.date;
+    const categories = postData.categories;
+    const slug = postData.slug
+    const acfFields = postData.acf
+
+    const post: Post = {
+        title, content, date, categories, slug, acfFields
+    }
+
+    return post as Post;
+}
